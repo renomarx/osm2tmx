3b7663c5d0fd6bb27f71c808e6976c6064f58b44
Renaud Marx <renaud.marx@adevinta.com>
2026-01-09 11:39:50 +0100

    csv_writer: remove unused function and add tests

--- pkg/tmx/csv_writer.go
+++ pkg/tmx/csv_writer.go
@@ -1,35 +1,11 @@
 package tmx
 
 import (
-	"encoding/csv"
 	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/renomarx/osm2tmx/pkg/model"
 )
-
-func PrintCSV(l *model.Layer) string {
-	var csvStr strings.Builder
-	writer := csv.NewWriter(&csvStr)
-
-	for y := range l.M {
-		var records []string = make([]string, len(l.M[y]))
-		for x, c := range l.M[y] {
-			var tile model.Tile = 0
-			if c != nil {
-				tile = c.Tile
-			}
-			records[x] = strconv.Itoa(int(tile))
-		}
-		err := writer.Write(records)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	return csvStr.String()
-}
 
 // PrintCSVWithLastComma writes non-standard csv,
 // adding a comma at the end of each line except the last line
--- pkg/tmx/csv_writer_test.go
+++ pkg/tmx/csv_writer_test.go
@@ -1,11 +1,36 @@
 package tmx
 
-import "testing"
+import (
+	"testing"
 
-func TestPrintCSV(t *testing.T) {
-	// TODO
-}
+	"github.com/renomarx/osm2tmx/pkg/model"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestPrintCSVWithLastComma(t *testing.T) {
-	// TODO
+	sizeY := 6
+	sizeX := 12
+	layer := model.Layer{
+		M: make([][]*model.Case, sizeY),
+	}
+	for y := range sizeY {
+		layer.M[y] = make([]*model.Case, sizeX)
+		for x := range sizeX {
+			layer.M[y][x] = &model.Case{
+				Tile: model.Tile(x * y % 3),
+				X:    x,
+				Y:    y,
+			}
+		}
+	}
+
+	csv := PrintCSVWithLastComma(&layer)
+	expectedCSV := `0,0,0,0,0,0,0,0,0,0,0,0,
+0,1,2,0,1,2,0,1,2,0,1,2,
+0,2,1,0,2,1,0,2,1,0,2,1,
+0,0,0,0,0,0,0,0,0,0,0,0,
+0,1,2,0,1,2,0,1,2,0,1,2,
+0,2,1,0,2,1,0,2,1,0,2,1
+`
+	assert.Equal(t, expectedCSV, csv)
 }
