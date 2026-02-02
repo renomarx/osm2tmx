2d1cc104014dc413ef525602de0776d55c9af68f
Renaud Marx <renaud.marx@adevinta.com>
2026-02-02 19:39:40 +0100

    wip: mapper from yaml file: add example and fix model

--- pkg/mapper/mapper_test.go
+++ pkg/mapper/mapper_test.go
@@ -1,14 +1,51 @@
 package mapper
 
 import (
+	"os"
 	"testing"
 
 	"github.com/paulmach/osm"
 	"github.com/renomarx/osm2tmx/pkg/model"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func TestMapper(t *testing.T) {
+
+	yamlFile, err := os.ReadFile("test/atlas-index.yaml")
+	require.NoError(t, err)
+
+	conf := Conf{}
+	err = yaml.Unmarshal(yamlFile, &conf)
+	assert.NoError(t, err)
+
+	expectedConf := Conf{
+		Tileset: Tileset{
+			Source:     "tileset/basechip_pipo.png",
+			TileWidth:  16,
+			TileHeight: 16,
+		},
+		Layers: 2,
+		Default: TileValue{
+			Tile: 2,
+			Altitude: &Altitude{
+				Min:  1400,
+				Tile: 1378,
+			},
+		},
+		Tags: map[string]TagsByLayer{
+			"aerialway": {
+				1: Tag{
+					TileValue: TileValue{
+						Tile: 647,
+					},
+				},
+			},
+		},
+		// TODO: other tags
+	}
+	assert.Equal(t, expectedConf, conf)
 
 	t.Run("correctly map single tag", func(t *testing.T) {
 		// TODO: table test for each tag
--- pkg/mapper/model.go
+++ pkg/mapper/model.go
@@ -1,9 +1,11 @@
 package mapper
 
 type Conf struct {
-	Tileset Tileset                `yaml:"tileset"`
-	Layers  int                    `yaml:"layers"`
-	Tags    map[string]TagsByLayer `yaml:"tags"`
+	Tileset     Tileset                `yaml:"tileset"`
+	Layers      int                    `yaml:"layers"`
+	Default     TileValue              `yaml:"default"`
+	Tags        map[string]TagsByLayer `yaml:"tags"`
+	CustomTiles map[int]CustomTile     `yaml:"custom_tiles,omitempty"`
 }
 
 type Tileset struct {
@@ -15,62 +17,63 @@ type Tileset struct {
 type TagsByLayer map[int]Tag
 
 type Tag struct {
-	TagTile
-	Values map[string]TilesByLayer `yaml:"values,omitempty"`
+	TileValue `yaml:",inline"`
+	Values    map[string]TilesByLayer `yaml:"values,omitempty"`
 }
 
-type TilesByLayer map[int]TagTile
-
-type TagTile struct {
-	Altitude *Altitude `yaml:"altitude,omitempty"`
-	Wall     *Wall     `yaml:"wall,omitempty"`
-	Position *Position `yaml:"position,omitempty"`
-}
-
-type Altitude struct {
-	TileValue
-	Min uint16 `yaml:"min"`
-}
-
-type Wall struct {
-	TileValue
-	Height int `yaml:"height"`
-	Pos    int `yaml:"pos"`
-}
-
-type Position struct {
-	Standalone         *PositionTile `yaml:"standalone"`
-	CornerTopLeft      *PositionTile `yaml:"corner_top_left"`
-	CornerTopRight     *PositionTile `yaml:"corner_top_right"`
-	CornerBottomLeft   *PositionTile `yaml:"corner_bottom_left"`
-	CornerBottomRight  *PositionTile `yaml:"corner_bottom_right"`
-	BorderTop          *PositionTile `yaml:"border_top"`
-	BorderBottom       *PositionTile `yaml:"border_bottom"`
-	BorderLeft         *PositionTile `yaml:"border_left"`
-	BorderRight        *PositionTile `yaml:"border_right"`
-	BorderLeftAndRight *PositionTile `yaml:"border_left_and_right"`
-	BorderTopAndBottom *PositionTile `yaml:"border_top_and_bottom"`
-	EndWayRight        *PositionTile `yaml:"end_way_right"`
-	EndWayLeft         *PositionTile `yaml:"end_way_left"`
-	EndWayBottom       *PositionTile `yaml:"end_way_bottom"`
-	EndWayTop          *PositionTile `yaml:"end_way_top"`
-}
-
-type PositionTile struct {
-	TileValue
-}
+type TilesByLayer map[int]TileValue
 
 type TileValue struct {
-	Tile   int           `yaml:"tile,omitempty"`
-	Random []RandomRange `yaml:"random,omitempty"`
+	Tile     int           `yaml:"tile,omitempty"`
+	Altitude *Altitude     `yaml:"altitude,omitempty"`
+	Random   []RandomRange `yaml:"random,omitempty"`
 }
 
 type RandomRange struct {
 	// Min is a percentage (0-100)
 	Min int `yaml:"min"`
 	// Max is a percentage (0-100)
-	Max  int `yaml:"max"`
-	Tile int `yaml:"tile,omitempty"`
+	Max      int       `yaml:"max"`
+	Tile     int       `yaml:"tile,omitempty"`
+	Altitude *Altitude `yaml:"altitude,omitempty"`
+}
+
+type Altitude struct {
+	Min  uint16 `yaml:"min"`
+	Tile int    `yaml:"tile,omitempty"`
+}
+
+type CustomTile struct {
+	Walls    []Wall    `yaml:"walls,omitempty"`
+	Position *Position `yaml:"position,omitempty"`
+}
+
+type Wall struct {
+	Height int `yaml:"height"`
+	Pos    int `yaml:"pos"`
+	Tile   int `yaml:"tile"`
+}
+
+type Position struct {
+	Standalone         *PositionTile `yaml:"standalone,omitempty"`
+	CornerTopLeft      *PositionTile `yaml:"corner_top_left,omitempty"`
+	CornerTopRight     *PositionTile `yaml:"corner_top_right,omitempty"`
+	CornerBottomLeft   *PositionTile `yaml:"corner_bottom_left,omitempty"`
+	CornerBottomRight  *PositionTile `yaml:"corner_bottom_right,omitempty"`
+	BorderTop          *PositionTile `yaml:"border_top,omitempty"`
+	BorderBottom       *PositionTile `yaml:"border_bottom,omitempty"`
+	BorderLeft         *PositionTile `yaml:"border_left,omitempty"`
+	BorderRight        *PositionTile `yaml:"border_right,omitempty"`
+	BorderLeftAndRight *PositionTile `yaml:"border_left_and_right,omitempty"`
+	BorderTopAndBottom *PositionTile `yaml:"border_top_and_bottom,omitempty"`
+	EndWayRight        *PositionTile `yaml:"end_way_right,omitempty"`
+	EndWayLeft         *PositionTile `yaml:"end_way_left,omitempty"`
+	EndWayBottom       *PositionTile `yaml:"end_way_bottom,omitempty"`
+	EndWayTop          *PositionTile `yaml:"end_way_top,omitempty"`
+}
+
+type PositionTile struct {
+	Tile int `yaml:"tile"`
 }
 
 // TODO: conf validation
